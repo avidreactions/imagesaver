8eaa65eb7bd870df011f457cc543986aa73369b3
Sean <writeseanhong@gmail.com>
2022-03-12 21:37:42 -0600

    update folder path and add download images function

--- app.py
+++ app.py
@@ -2,19 +2,32 @@ import os
 import requests
 from bs4 import *
 
-def create_folder():
-  folder_path = "imagesaver/"
+def download_images(images, full_folder_path):
+  count = 0;
+  print(f"total {len(images)} have been found!");
+  print(f"The images will be saved to {full_folder_path}");
+
+def create_folder(images):
   try:
-    folder_name = input("Enter Folder Name:- ")
-    os.makedirs(folder_path + folder_name)
+    folder_name = input("Enter Folder Name: ")
+    full_folder_path = "imagesave/" + folder_name;
+    os.makedirs(full_folder_path + folder_name);
 
   except:
     print("Folder Exist with that name!")
     create_folder()
+  download_images(images, full_folder_path)
 
 def grab_images(url):
   print(f"grabbing images from {url}");
-  create_folder();
+  
+  url_request = requests.get(url);
+
+  soup = BeautifulSoup(url_request.text, "html.parser");
+
+  images = soup.findAll('img');
+
+  create_folder(images);
 
 url = input('Please enter url: ');
 

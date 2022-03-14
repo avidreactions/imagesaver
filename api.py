271a47c48a042c83a1ea44c10f28cc0b6da691cb
Sean <writeseanhong@gmail.com>
2022-03-14 01:46:55 -0500

    add post method to imagesaver api

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template, request;
 from app import grab_images;
 
 app = Flask(__name__);
@@ -11,13 +11,12 @@ def page_not_found(e):
 def home():
   return "<h1>Hello world</h1>";
 
-@app.route("/about")
-def about():
-  return "<h1>This is the about page</h1>"
-
-@app.route("/imagesaver")
+@app.route("/imagesaver", methods=["GET","POST"])
 def imagesaver():
-  grab_images("https://www.ufc.com");
+  if request.method == "POST":
+    print("printing method is post, run get_images", request.method);
+    
+  # grab_images("https://www.ufc.com");
   return "<h1>RUNNING IMAGE SAVER</h1>"
 
 if __name__ == "__main__":
